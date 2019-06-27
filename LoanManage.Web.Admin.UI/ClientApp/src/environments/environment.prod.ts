da7fe58446fd605aff0beaefe431f712b2ca074c
rowshankibria <rowshan.kibria@gmail.com>
2019-06-28 00:17:14 +0600

    delete admin site

--- LoanManage.Web.Admin.UI/ClientApp/src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-    production: true,
-    apiUrl: "http://loanapi.pyxidaapps.com/api/",
-    rootUrl: "http://loanapi.pyxidaapps.com/",
-    identityUrl: "http://hbapi.dev.itmagnet.biz/api/client-application/admin-application-angular-client"
-};
